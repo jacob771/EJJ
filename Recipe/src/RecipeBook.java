38622549d36d439cba8af2c4184d4e2217da8490
jacob771 <ss10368@nyu.edu>
2020-10-08 02:24:38 +0900

    updated

--- Recipe/src/RecipeBook.java
+++ Recipe/src/RecipeBook.java
@@ -2,21 +2,23 @@ import java.util.*;
 import java.io.*;
 
 public class RecipeBook {
-    public RecipeBook() {
-        ArrayList<Recipe> recipeBook = new ArrayList<Recipe>();
+    ArrayList<Recipe> recipeBook = new ArrayList<Recipe>();
+
+    public RecipeBook(ArrayList<Recipe> recipeBook) {
+        this.recipeBook = recipeBook;
     }
 
     /**
      * Put Certain Input, then it searches the Recipe.
      * */
-    private searchRecipeBook() {
+    private Recipe searchRecipeBook(String name) {
 
     }
 
     /**
      * Add new Recipe
      * */
-    private addRecipe() {
-
+    private void addRecipe(Recipe recipe) {
+        recipeBook.add(recipe);
     }
 }
